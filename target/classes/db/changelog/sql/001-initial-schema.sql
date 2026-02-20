db6efb4abcc4c88c2845c93e445b8c87d66175e4
Ayezah Maryam <ayeza.work43@gmail.com>
2026-02-20 12:11:22 +0500

    removed target folder from gitignore file

---
+++ target/classes/db/changelog/sql/001-initial-schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(150) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    role TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO users (username, password, role)
+VALUES
+('admin', 'admin123', "ADMIN");
