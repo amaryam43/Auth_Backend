5bdfb85b00221b415d2f4ee9475d712ef3fe86c9
Ayezah Maryam <ayeza.work43@gmail.com>
2026-02-24 11:05:25 +0500

    fix

--- src/main/java/org/example/service/MyUserDetailsService.java
+++ src/main/java/org/example/service/MyUserDetailsService.java
@@ -6,7 +6,9 @@ import org.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
